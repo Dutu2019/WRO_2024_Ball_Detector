fc895cdecce6eb90f32f2a94397673e722837e3c
Dutu2019 <erictanase2007@gmail.com>
2024-07-09 14:03:47 -0400

    Removed AI model and made self-contained executable.

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,27 +1,33 @@
 #include <stdio.h>
+#include <iostream>
 #include <chrono>
-#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
 using namespace std;
 
 float distance(float x1, float x2, float y1, float y2);
+void incrementFrame();
 
+int frameCount = 0;
+auto frameStart = chrono::high_resolution_clock::now();
 int main(int argc, char **argv) {
     int frameWidth = 48;
     int frameHeight = 48;
 
-    cv::Scalar orangeLow = cv::Scalar(22 / 2, 70.0 / 100.0 * 255, 45.0 / 100.0 * 255);
-    cv::Scalar orangeHigh = cv::Scalar(26 / 2, 90.0 / 100.0 * 255, 90.0 / 100.0 * 255);
-    cv::Mat colorFrame, grayFrame, hsvFrame, mask;
+    cv::Scalar orangeLow = cv::Scalar(33.0 / 2.0, 70.0 / 100.0 * 255, 50.0 / 100.0 * 255);
+    cv::Scalar orangeHigh = cv::Scalar(42.0 / 2.0, 100.0 / 100.0 * 255, 100.0 / 100.0 * 255);
+    cv::Mat colorFrame, hsvFrame, mask;
     cv::VideoCapture camera;
     camera.open(0);
 
      //--- GRAB AND WRITE LOOP
     cout << "Start grabbing" << endl
     << "Press any key to terminate" << endl;
-    auto start = chrono::high_resolution_clock::now();
+
     for (;;)
     {   
-        auto frameStart = chrono::high_resolution_clock::now();
         camera.read(colorFrame);
         // check if we succeeded
         if (colorFrame.empty()) {
@@ -30,30 +36,38 @@ int main(int argc, char **argv) {
         }
 
         // Apply filters to video stream
+        cv::GaussianBlur(colorFrame, colorFrame, cv::Size(17, 17), 1.3, 0.0);
         cv::cvtColor(colorFrame, hsvFrame, cv::COLOR_BGR2HSV);
 
+        // Get Bounding boxes of balls
         vector<vector<cv::Point>> contours;
         cv::inRange(hsvFrame, orangeLow, orangeHigh, mask);
         cv::findContours(mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-        // cv::drawContours(colorFrame, contours, -1, (255, 0, 0), 3);
         for (int i = 0; i < contours.size(); i++) {
             cv::Rect boundRect = cv::boundingRect(contours[i]);
             if (boundRect.area() > 270) {
                 cv::rectangle(colorFrame, boundRect.tl(), boundRect.br(), (255, 0, 0), 3);
             }
         }
-        if (chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - start).count() > 1000) {
-            auto stop = chrono::high_resolution_clock::now();
-            auto duration = chrono::duration_cast<chrono::milliseconds>(stop - frameStart);
-            start = stop;
-            printf("FPS: %d\n", 1000/duration.count());
-        }
+
+        incrementFrame();
         cv::imshow("Live", colorFrame);
         if (cv::waitKey(1) >= 0) break;
     }
     camera.release();
     cv::destroyAllWindows();
     return 0;
+}
+
+void incrementFrame() {
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - frameStart);
+    if (duration.count() >= 1000) {
+        printf("FPS: %d\n", frameCount);
+        frameCount = 0;
+        frameStart = stop;
+    }
+    frameCount++;
 }
 
 float distance(float x1, float x2, float y1, float y2) {
